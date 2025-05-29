0726e7bea618aef6ff1ad4ca029c6674e8187a84
Suhyun Park <me@shiftpsh.com>
2025-05-29 23:38:35 +0900

    fix: next.js bug with images

--- components/mdx/ContentImage.tsx
+++ components/mdx/ContentImage.tsx
@@ -1,10 +1,20 @@
 import styled from "@emotion/styled";
+import { usePathname } from "next/navigation";
 
-const ContentImage = styled.img`
+const ContentImageComponent = styled.img`
   max-width: 100%;
   height: auto;
   border-radius: 8px;
   box-shadow: ${({ theme }) => theme.styles.shadow(undefined, "16px")};
 `;
+
+const ContentImage = (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+  const { src, ...rest } = props;
+  const pathname = usePathname();
+  const resolvedSrc = src?.startsWith(".")
+    ? `${pathname}/${src.slice(1)}`
+    : src;
+  return <ContentImageComponent src={resolvedSrc} {...rest} />;
+};
 
 export default ContentImage;
